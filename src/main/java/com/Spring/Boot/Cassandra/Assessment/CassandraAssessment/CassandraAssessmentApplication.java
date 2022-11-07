3bf3b5a1e45afc1422e13d7424af5e6d10a0668e
BY-MANISH-KUSHWAHA <manish.kushwaha@blueyonder.com>
2022-11-07 09:40:03 +0530

    First

---
+++ src/main/java/com/Spring/Boot/Cassandra/Assessment/CassandraAssessment/CassandraAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.Boot.Cassandra.Assessment.CassandraAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraAssessmentApplication.class, args);
+	}
+
+}
